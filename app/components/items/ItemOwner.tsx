dc61df4f95e0e8d58be9fa7e25599a8f04db31fb
yi-cheng-liu <liuyiche@umich.edu>
2023-07-05 18:28:02 -0400

    add functions such as updating the information

--- app/components/items/ItemOwner.tsx
+++ app/components/items/ItemOwner.tsx
@@ -8,16 +8,35 @@ import {
 } from "react-icons/md";
 import Container from "../Container";
 import Heading from "../Heading";
+import { useState } from "react";
+import Button from "../Button";
 
 interface ItemOwnerProps {
   user: SafeUser;
   heading?: string;
+  onUpdateUser: (data: any) => void;
 }
 
-const ItemOwner: React.FC<ItemOwnerProps> = ({user, heading}) => {
+const ItemOwner: React.FC<ItemOwnerProps> = ({ user, heading, onUpdateUser }) => {
+  const [phone, setPhone] = useState(user?.phone || "");
+  const [pickupAddress, setPickupAddress] = useState(user?.pickupAddress || "");
+  const [finalPickupDate, setFinalPickupDate] = useState(
+    user?.finalPickupDate?.toISOString() || ""
+  );
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const data = {
+      phone,
+      pickupAddress,
+      finalPickupDate,
+    };
+    onUpdateUser(data);
+  };
+
   return (
     <div className="py-6">
-      {heading && (<Heading title={heading} />)}
+      {heading && <Heading title={heading} />}
       <div className="flex flex-col gap-6">
         {/* Name and Avatar */}
         <div className="text-xl font-semibold flex flex-row items-end gap-2">
@@ -25,38 +44,64 @@ const ItemOwner: React.FC<ItemOwnerProps> = ({user, heading}) => {
           <div className="flex text-lg font-semibold">{user?.name}</div>
         </div>
 
-        {/* Contact Information */}
-        <div className="flex flex-col gap-4">
-          {user.phone && (
-            <div className="flex items-center gap-2">
-              <MdOutlinePhone size={24} />
-              <div>{user?.phone}734-800-6477</div>
-            </div>
-          )}
-          <div className="flex items-center gap-2">
-            <MdOutlineMailOutline size={24} />
-            <div>{user.email}</div>
-          </div>
-          {user.pickupAddress && (
-            <div className="flex items-center gap-2">
-              <MdOutlineHouse size={24} />
-              <div>
-                {user.pickupAddress}Apt 808b, 2200 Fuller Ct., Ann Arbor, MI
-              </div>
-            </div>
-          )}
-          {user.finalPickupDate && (
-            <div className="flex items-center gap-2">
-              <MdOutlineCalendarToday size={24} />
-              <div className="flex flex-row justify-start items-center gap-2">
-                <div className="">Pick up before</div>
-                <div className="font-extrabold text-rose-500">
-                  {user.finalPickupDate.toString()}2023-2-4
+        <form onSubmit={handleSubmit}>
+          {/* Contact Information */}
+          {/* Phone */}
+          <div className="flex flex-col gap-4">
+            {user.phone && (
+              <div className="flex items-center gap-2">
+                <MdOutlinePhone size={24} />
+                <div>
+                  <input
+                    type="text"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                  />
                 </div>
               </div>
+            )}
+
+            {/* Email */}
+            <div className="flex items-center gap-2">
+              <MdOutlineMailOutline size={24} />
+              <div>{user.email}</div>
             </div>
-          )}
-        </div>
+
+            {/* Pickup Address */}
+            {user.pickupAddress && (
+              <div className="flex items-center gap-2">
+                <MdOutlineHouse size={24} />
+                <div>
+                  <input
+                    type="text"
+                    value={pickupAddress}
+                    onChange={(e) => setPickupAddress(e.target.value)}
+                    className="w-full"
+                  />
+                </div>
+              </div>
+            )}
+
+            {/* Final Pickup Date */}
+            {user.finalPickupDate && (
+              <div className="flex items-center gap-2">
+                <MdOutlineCalendarToday size={24} />
+                <div className="flex flex-row justify-start items-center gap-2">
+                  <div className="">Pick up before</div>
+                  <div className="font-extrabold text-rose-500">
+                    <input
+                      type="date"
+                      value={finalPickupDate}
+                      onChange={(e) => setFinalPickupDate(e.target.value)}
+                    />
+                  </div>
+                </div>
+              </div>
+            )}
+
+            <Button label="Update" onClick={handleSubmit} />
+          </div>
+        </form>
       </div>
     </div>
   );
--- app/profile/ProfileClient.tsx
+++ app/profile/ProfileClient.tsx
@@ -4,17 +4,34 @@ import { SafeUser } from "@/app/types";
 
 import Container from "../components/Container";
 import ItemOwner from "../components/items/ItemOwner";
-
+import axios from "axios";
+import { toast } from "react-hot-toast";
 
 interface ProfileClientProps {
   currentUser: SafeUser;
 }
 
 const ProfileClient: React.FC<ProfileClientProps> = ({ currentUser }) => {
+  const handleUpdateUser = async (data: any) => {
+    try {
+      // Make the API call to update the user's information
+      await axios.post("/api/profile", data);
+      // Show a success message
+      toast.success("Profile updated successfully");
+    } catch (err) {
+      // Show an error message
+      toast.error("An error occurred while updating your profile");
+    }
+  };
+
   return (
     <Container>
-      <ItemOwner user={currentUser} heading="Profile" />
+      <ItemOwner
+        user={currentUser}
+        heading="Profile"
+        onUpdateUser={handleUpdateUser}
+      />
     </Container>
   );
-}
+};
 export default ProfileClient;
