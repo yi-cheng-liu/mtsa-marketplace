862465c1e6d328d1cbf5123dd201e3fedaa08102
yi-cheng-liu <liuyiche@umich.edu>
2023-08-16 18:23:38 -0400

    add privacy policy with different language

---
+++ app/components/about/privacy-policy.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const PrivacyPolicy: React.FC = () => {
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+
+      <h3>EN</h3>
+      <p>
+        Our website may, with your consent, publish certain information from our
+        website to your social media activity page through Facebook or similar
+        social networking services. If you disagree with such postings, please
+        do not click the agree button. Alternatively, you can later remove such
+        information or decline further postings from our website through the
+        membership mechanisms of the respective social networking services.
+      </p>
+      <p>
+        If you have any questions, you can still contact our website, and we
+        will assist you in verifying and addressing related issues.
+      </p>
+      <p>
+        If the delivery address, contact person, and contact method you provided
+        are not your personal information, you agree that you have obtained the
+        consent of the respective parties to provide it to our website and have
+        informed them of the legal notification on behalf of our website.
+      </p>
+      <p>
+        Except as required by law to provide to the judiciary, investigative
+        agencies, relevant authorities, or to collaborate with our partners for
+        necessary activities, our website will not arbitrarily provide your
+        personal data to third parties.
+      </p>
+
+      <h3>CN</h3>
+      <>
+        本網站可能透過 Facebook
+        或類似社群服務系統，於取得您的同意後，將部分本網站的資訊發布於您的社群活動資訊頁面。若您不同意該等訊息之發布，請您勿點選同意鍵，或於事後透過各該社群服務之會員機制移除該等資訊或拒絕本網站繼續發布相關訊息。
+      </>
+      <p>若有任何問題，仍可與本網站聯絡，本網站將協助您確認、處理相關問題。</p>
+      <p>
+        若您所填寫之送貨地址、聯絡人、聯絡方式等非您本人之個人資料，您同意已取得各該當事人之同意提供予本網站，並已代本網站就前開法定告知事項予以告知。
+      </p>
+      <p>
+        除依法應提供予司法、檢調機關、相關主管機關，或與本網站協力廠商為執行相關活動必要範圍之利用外，本網站將不會任意將您的個人資料提供予第三人。
+      </p>
+    </div>
+  )
+}
+
+export default PrivacyPolicy
