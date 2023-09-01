771bf84f427a5bd8c263616f358df3a082cf3b59
yi-cheng-liu <liuyiche@umich.edu>
2023-09-01 00:12:57 -0400

    modify properties client

--- app/profile/PropertiesClient.tsx
+++ app/profile/PropertiesClient.tsx
@@ -5,11 +5,10 @@ import axios from "axios";
 import { useCallback, useState } from "react";
 import { useRouter } from "next/navigation";
 
-import { SafeItem, SafeReservation, SafeUser } from "@/app/types";
+import { SafeItem, SafeUser } from "@/app/types";
 import Heading from "@/app/components/Heading";
 import Container from "@/app/components/Container";
 import ItemsCard from "@/app/components/items/ItemsCard";
-import EmptyState from '@/app/components/EmptyState'
 import { CardMode, EmptyStateMode } from '../types/constants'
 
 
@@ -51,7 +50,6 @@ const PropertiesClient: React.FC<PropertiesClientProps> = ({
     <Container>
       <div className="py-6">
         <Heading title="ALL ITEMS" />
-        {items && items.length > 0 ? (
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2 md:gap-4 xl:gap-6">
             {items &&
               items.map((item: any) => (
@@ -67,12 +65,6 @@ const PropertiesClient: React.FC<PropertiesClientProps> = ({
                 />
               ))}
           </div>
-        ) : (
-            <EmptyState
-              title="No Items"
-              mode={EmptyStateMode.FULL_PAGE}
-            />
-        )}
       </div>
     </Container>
   )
