a76442bca3293d1e29ecaa71b8e2bff870c8c68e
yi-cheng-liu <111710682+yi-cheng-liu@users.noreply.github.com>
2023-09-07 00:46:32 -0400

    change back to fill

--- app/components/items/ItemsCard.tsx
+++ app/components/items/ItemsCard.tsx
@@ -69,14 +69,10 @@ const ItemsCard: React.FC<ItemsCardProps> = ({ data, reservation, currentUser, d
         >
           <div className="aspect-square w-full relative overflow-hidden rounded-2xl">
             <Image
+              fill
               alt="item image"
               src={data.image}
-              style={{objectFit:"cover"}}
-              width={640}  // maximum image width
-              height={480} // maximum image height
-              sizes="(max-width: 640px) 100vw, 640px"
               className="object-cover h-full w-full group-hover:scale-110 transition"
-              priority
             />
             {isSold && (
               <div className="absolute inset-0 flex items-center justify-center">
